52d834ce7d4e1258d287e2e29f9ecae6ff3027ac
keisuke <keisukent0228@i.softbank.jp>
2020-12-29 12:09:26 +0900

    two_drow_cardメソッドの作成

--- main.rb
+++ main.rb
@@ -11,38 +11,46 @@ class Card
     end
     @card
   end
-end
 
-class Player 
-  def first_drow_card(dealer_first_drew_card)
-    
-  end
-end
-
-class Dealer 
-  def first_drow_card(created_card)
-    drew_card = []
-    puts "ディーラーはカードを2枚引きました。"
+  def two_drow_card(remaining_card)
+    @drew_card = []
     i = -1
     2.times do 
       i += 1
       argument_num = rand(51 - i)
-      drew_card << created_card[argument_num]
-      created_card.delete_at(argument_num)
+      @drew_card << remaining_card[argument_num]
+      remaining_card.delete_at(argument_num)
     end 
-    created_card
+    remaining_card
   end
 end
+
+class Dealer < Card
+  def first_drow_card(remaining_card)
+    puts "ディーラーはカードを2枚引きました。"
+    two_drow_card(remaining_card)
+    puts @drew_card
+    remaining_card
+  end
+end
+
+class Player < Card
+  def first_drow_card(dealer_first_drew_card)
+    two_drow_card(dealer_first_drew_card)
+  end
+end
+
+
 
 pictures = ["♤", "♧", "♡", "♢"]
 letters = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 card = Card.new
-created_card = card.create_card(pictures, letters)
+remaining_card = card.create_card(pictures, letters)
 player = Player.new
 dealer = Dealer.new
 
-dealer_first_drew_card = dealer.first_drow_card(created_card)
+dealer_first_drew_card = dealer.first_drow_card(remaining_card)
 player_first_drew_card = player.first_drow_card(dealer_first_drew_card)
 
 #dealerのfirst_drow_cardが途中です
