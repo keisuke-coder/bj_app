13c29837e656636972aae76067695c3ccc88cfe0
keisuke <keisukent0228@i.softbank.jp>
2020-12-29 12:24:39 +0900

    two_drow_cardメソッドの引数をremaining_cardに変更

--- main.rb
+++ main.rb
@@ -1,15 +1,14 @@
 class Card
-  attr_reader :card
-  attr_writer :card
+  attr_reader :remaining_card
   def create_card(pictures, letters)
-    @card = []
+    @remaining_card = []
     letters.each.with_index(1) do |letter, i|
       i = 10 if i > 10 
       pictures.each do |picture|
-        @card << {picture: picture, letter: letter, point: i}
+        @remaining_card << {picture: picture, letter: letter, point: i}
       end
     end
-    @card
+    @remaining_card
   end
 
   def two_drow_card(remaining_card)
@@ -30,13 +29,13 @@ class Dealer < Card
     puts "ディーラーはカードを2枚引きました。"
     two_drow_card(remaining_card)
     puts @drew_card
-    remaining_card
   end
 end
 
 class Player < Card
-  def first_drow_card(dealer_first_drew_card)
-    two_drow_card(dealer_first_drew_card)
+  def first_drow_card(remaining_card)
+    two_drow_card(remaining_card)
+    puts @drew_card
   end
 end
 
@@ -46,13 +45,13 @@ pictures = ["♤", "♧", "♡", "♢"]
 letters = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 card = Card.new
-remaining_card = card.create_card(pictures, letters)
+card.create_card(pictures, letters)
 player = Player.new
 dealer = Dealer.new
 
-dealer_first_drew_card = dealer.first_drow_card(remaining_card)
-player_first_drew_card = player.first_drow_card(dealer_first_drew_card)
-
+puts "ゲーム開始です!"
+dealer_first_drew_card = dealer.first_drow_card(card.remaining_card)
+player_first_drew_card = player.first_drow_card(card.remaining_card)
 #dealerのfirst_drow_cardが途中です
 
 
