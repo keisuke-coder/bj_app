fa13a12b45c64291d0a280d96adb3ef037d83ca3
keisuke <keisukent0228@i.softbank.jp>
2020-12-29 10:35:46 +0900

    dealerのfirst_drow_cardメソッドの作成

--- main.rb
+++ main.rb
@@ -9,19 +9,42 @@ class Card
         @card << {picture: picture, letter: letter, point: i}
       end
     end
+    @card
   end
 end
 
-class Player
-
+class Player 
+  def first_drow_card(dealer_first_drew_card)
+    
+  end
 end
 
-class Dealer
-
+class Dealer 
+  def first_drow_card(created_card)
+    drew_card = []
+    puts "ディーラーはカードを2枚引きました。"
+    i = -1
+    2.times do 
+      i += 1
+      argument_num = rand(51 - i)
+      drew_card << created_card[argument_num]
+      created_card.delete_at(argument_num)
+    end 
+    created_card
+  end
 end
 
 pictures = ["♤", "♧", "♡", "♢"]
 letters = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 card = Card.new
-card.create_card(pictures, letters)
+created_card = card.create_card(pictures, letters)
+player = Player.new
+dealer = Dealer.new
+
+dealer_first_drew_card = dealer.first_drow_card(created_card)
+player_first_drew_card = player.first_drow_card(dealer_first_drew_card)
+
+#dealerのfirst_drow_cardが途中です
+
+
