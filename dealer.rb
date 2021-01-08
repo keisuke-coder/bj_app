46f49d39f3803f5689babe6c95158768f46dedee
keisuke <keisukent0228@i.softbank.jp>
2021-01-08 17:45:16 +0900

    定数の導入

--- dealer.rb
+++ dealer.rb
@@ -1,3 +1,5 @@
+END_POINT = 17
+
 class Dealer < Card
   def first_drow(remaining_card)
     puts "ディーラーはカードを2枚引きました。"
@@ -8,7 +10,7 @@ class Dealer < Card
     puts "*=*=*=*=*=*=*=*=*==*=*=*=*"
     print "ディーラーのカード "
     disp_have_card
-    while @total_point < 17
+    while @total_point < END_POINT
       one_drow_card(remaining_card)  
     end
   end
